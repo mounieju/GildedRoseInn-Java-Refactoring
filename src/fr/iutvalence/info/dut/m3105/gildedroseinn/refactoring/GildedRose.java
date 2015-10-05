93f304c241238c46bb1ce1e268ae780b8a00c086
mounieju <mounieju@PC-B124-01.iut-valence.fr>
2015-10-05 13:47:49 +0200

    Update 1

--- src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
+++ src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
@@ -24,84 +24,108 @@ public class GildedRose
 		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
 		items.add(new Item("Conjured Mana Cake", 3, 6));
 
-		updateQuality();
+		updateQualityAndSellInForAllItems();
 	}
 
-	public static void updateQuality()
+	public static void updateQualityAndSellInForAllItems()
 	{
-		for (int i = 0; i < items.size(); i++)
+		for (int currentItem = 0; currentItem < items.size(); currentItem++)
 		{
-			if ((!"Aged Brie".equals(items.get(i).getName()))
-					&& !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
+			Item item = items.get(currentItem);
+			updateQualityAndSellInForItem(item);
+		}
+	}
+
+	
+	private static void updateQualityAndSellInForItem(Item item) 
+	{
+		if (!isInalterable(item))
+		{
+			updateQuality(item);
+			updateSellIn(item);
+
+			if (item.getSellIn() < 0)
 			{
-				if (items.get(i).getQuality() > 0)
-				{
-					if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-					{
-						items.get(i).setQuality(items.get(i).getQuality() - 1);
-					}
-				}
+				updateQualityOfExpiredItem(item);
 			}
-			else
+		}		
+	}
+
+	
+	private static void updateQuality(Item item) 
+	{
+		if ((!"Aged Brie".equals(item.getName()))
+				&& !"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+		{
+			if (item.getQuality() > 0)
 			{
-				if (items.get(i).getQuality() < 50)
-				{
-					items.get(i).setQuality(items.get(i).getQuality() + 1);
-
-					if ("Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-					{
-						if (items.get(i).getSellIn() < 11)
-						{
-							if (items.get(i).getQuality() < 50)
-							{
-								items.get(i).setQuality(items.get(i).getQuality() + 1);
-							}
-						}
-
-						if (items.get(i).getSellIn() < 6)
-						{
-							if (items.get(i).getQuality() < 50)
-							{
-								items.get(i).setQuality(items.get(i).getQuality() + 1);
-							}
-						}
-					}
-				}
+					item.setQuality(item.getQuality() - 1);
 			}
-
-			if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
+		}
+		else
+		{
+			if (item.getQuality() < 50)
 			{
-				items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-			}
+				item.setQuality(item.getQuality() + 1);
 
-			if (items.get(i).getSellIn() < 0)
-			{
-				if (!"Aged Brie".equals(items.get(i).getName()))
+				if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
 				{
-					if (!"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
+					if (item.getSellIn() < 11)
 					{
-						if (items.get(i).getQuality() > 0)
+						if (item.getQuality() < 50)
 						{
-							if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-							{
-								items.get(i).setQuality(items.get(i).getQuality() - 1);
-							}
+							item.setQuality(item.getQuality() + 1);
 						}
 					}
-					else
+
+					if (item.getSellIn() < 6)
 					{
-						items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
-					}
-				}
-				else
-				{
-					if (items.get(i).getQuality() < 50)
-					{
-						items.get(i).setQuality(items.get(i).getQuality() + 1);
+						if (item.getQuality() < 50)
+						{
+							item.setQuality(item.getQuality() + 1);
+						}
 					}
 				}
 			}
 		}
 	}
 
+	
+
+	private static void updateQualityOfExpiredItem(Item item) 
+	{
+		if (!"Aged Brie".equals(item.getName()))
+		{
+			if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+			{
+				if (item.getQuality() > 0)
+				{				
+						item.setQuality(item.getQuality() - 1);
+				}
+			}
+			else
+			{
+				item.setQuality(item.getQuality() - item.getQuality());
+			}
+		}
+		else
+		{
+			if (item.getQuality() < 50)
+			{
+				item.setQuality(item.getQuality() + 1);
+			}
+		}
+	}
+
+	
+	private static void updateSellIn(Item item) 
+	{
+			item.setSellIn(item.getSellIn() - 1);
+	}
+
+	
+	private static boolean isInalterable(Item item) 
+	{
+		return "Sulfuras, Hand of Ragnaros".equals(item.getName());
+	}
 }
